3497e7f320f4b0668b32d68613336fd7fcbf6fb8
DevSBejaranoM <dev.sbejaranom@gmail.com>
2024-06-05 07:25:42 +0200

    data/postgres to data/postgresql

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { prisma } from "../../data/postgres";
+import { prisma } from "../../data/postgreSql";
 import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
 
 export class TodosController {
