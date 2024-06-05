a1a7e610703b1384691e88af2f8db706a9dc0326
DevSBejaranoM <dev.sbejaranom@gmail.com>
2024-06-05 20:38:49 +0200

    Apply use-cases

---
+++ src/domain/datasource/todo.datasource.ts
@@ -0,0 +1,11 @@
+import { CreateTodoDto, UpdateTodoDto } from "../dtos";
+import { TodoEntity } from "../entities/todo.entity";
+
+export abstract class TodoDatasource {
+  abstract create(createTodoDto: CreateTodoDto): Promise<TodoEntity>;
+  //todo: paginación
+  abstract getAll(): Promise<TodoEntity[]>;
+  abstract findById(id: number): Promise<TodoEntity | null>;
+  abstract updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity | null>;
+  abstract deleteById(id: number): Promise<TodoEntity>;
+}
